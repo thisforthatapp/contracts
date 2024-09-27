1b16b35fd7321405fd872ff4201b9b492bbb9d5f
wd021 <dannywitters@gmail.com>
2024-09-27 13:32:50 +0100

    add openzeppelin contracts

--- src/V1EscrowMultiChain.sol
+++ src/V1EscrowMultiChain.sol
@@ -1,4 +1,6 @@
-/* TODO: rewrite cross chain implementation to minimize onchain footprint. Users are already relying on trust of TFT. Minimize the gas. */
+/* TODO: 
+    rewrite cross chain implementation to minimize onchain footprint. Users are already relying on trust of TFT. Minimize the gas.
+*/
 
 // SPDX-License-Identifier: MIT
 /*
